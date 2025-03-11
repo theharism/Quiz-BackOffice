62fdd2ee44c6243eda98c66341806f7215657ddc
theharism <chharis9999@gmail.com>
2025-03-12 00:01:08 +0500

    protected admin routes

--- routes/questionare.route.js
+++ routes/questionare.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const questionController = require('../controllers/questionare.controller');
+const { Verify, VerifyRole } = require('../middlewares/authenticationMiddleware');
 
 router.get('/', questionController.getAllQuestions);
 router.get('/:id', questionController.getQuestionById);
-router.post('/', questionController.createQuestion);
-router.put('/:id', questionController.updateQuestion);
-router.delete('/:id', questionController.deleteQuestion);
+router.post('/', Verify, VerifyRole, questionController.createQuestion);
+router.put('/:id', Verify, VerifyRole, questionController.updateQuestion);
+router.delete('/:id', Verify, VerifyRole, questionController.deleteQuestion);
 
 module.exports = router;
--- routes/quizResult.route.js
+++ routes/quizResult.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const quizResultController = require('../controllers/quizResult.controller');
+const { Verify, VerifyRole } = require('../middlewares/authenticationMiddleware');
 
-router.get('/', quizResultController.getAllQuizResults);
-router.get('/:id', quizResultController.getQuizResultById);
+router.get('/',Verify,VerifyRole, quizResultController.getAllQuizResults);
+router.get('/:id',Verify,VerifyRole, quizResultController.getQuizResultById);
 router.post('/', quizResultController.createQuizResult);
-router.put('/:id', quizResultController.updateQuizResult);
-router.delete('/:id', quizResultController.deleteQuizResult);
+router.put('/:id',Verify,VerifyRole, quizResultController.updateQuizResult);
+router.delete('/:id',Verify,VerifyRole, quizResultController.deleteQuizResult);
 
 module.exports = router;
